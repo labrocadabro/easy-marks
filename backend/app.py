fc52dc7957827c1b9536e3455e440b0779661720
Laura Abro <l.abrocadabro@gmail.com>
2024-03-10 12:27:12 -0300

    set up for heroku

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
+
+
+@app.get("/api")
+def api():
+    return "api"
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return app.send_static_file("index.html")
