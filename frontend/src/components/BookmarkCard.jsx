cae61d415cff7a83f349b487fe002afcad68ef30
MichelThieR <mtrosemond@gmail.com>
2024-03-12 17:35:02 -0300

    added link in card

--- frontend/src/components/BookmarkCard.jsx
+++ frontend/src/components/BookmarkCard.jsx
@@ -1,14 +1,16 @@
 function BookmarkCard() {
 	return (
-		<div className="mt-8 rounded overflow-hidden shadow-md w-full sm:w-4/12">
-			<img src="google.png" className="w-full object-cover" />
-			<div className="text-gray-600 m-4">
-				{/* Title */}
-				<h2 className="mb-2">Google</h2>
-				{/* Description */}
-				<div className="block">That's where you find shit</div>
+		<a href="https://www.google.com/" target="_blank">
+			<div className="mt-8 rounded overflow-hidden shadow-md w-full sm:w-4/12">
+				<img src="google.png" className="w-full object-cover" />
+				<div className="text-gray-600 m-4">
+					{/* Title */}
+					<h2 className="mb-2">Google</h2>
+					{/* Description */}
+					<div className="block">That's where you find shit</div>
+				</div>
 			</div>
-		</div>
+		</a>
 	);
 }
 
