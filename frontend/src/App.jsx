3905d907494be1f6d62ed14ba5fbd2feff454d3a
Laura Abro <l.abrocadabro@gmail.com>
2024-03-23 16:33:43 -0300

    auth complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { UserContext } from "./context/UserContext";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import HomePage from "./pages/HomePage";
@@ -7,18 +9,63 @@ import AddBookmarkPage from "./pages/AddBookmarkPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [photo, setPhoto] = useState("");
+	const [firstName, setFirstName] = useState("");
+
+	useEffect(() => {
+		const accessToken = window.sessionStorage.getItem("accessToken");
+		console.log(accessToken);
+		if (accessToken) {
+			fetch("http://127.0.0.1:5000/session", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${accessToken}`,
+				},
+				body: JSON.stringify({
+					userId: window.sessionStorage.getItem("userId"),
+				}),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					if (data?.valid) {
+						setLoggedIn(true);
+						setPhoto(data.photo);
+						setFirstName(data.firstName);
+						// access token may have changed
+						window.sessionStorage.setItem("accessToken", data.accessToken);
+					} else {
+						window.sessionStorage.removeItem("accessToken");
+						window.sessionStorage.removeItem("userId");
+					}
+				});
+		}
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
 	return (
 		<>
-			<Header />
-			<main className="px-8 py-6 h-full">
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/bookmarks" element={<BookmarksPage />} />
-					<Route path="/add" element={<AddBookmarkPage />} />
-					<Route path="*" element={<NotFoundPage />} />
-				</Routes>
-			</main>
-			<Footer />
+			<UserContext.Provider
+				value={[
+					loggedIn,
+					setLoggedIn,
+					photo,
+					setPhoto,
+					firstName,
+					setFirstName,
+				]}
+			>
+				<Header />
+				<main className="px-8 py-6 h-full">
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/bookmarks" element={<BookmarksPage />} />
+						<Route path="/add" element={<AddBookmarkPage />} />
+						<Route path="*" element={<NotFoundPage />} />
+					</Routes>
+				</main>
+				<Footer />
+			</UserContext.Provider>
 		</>
 	);
 }
