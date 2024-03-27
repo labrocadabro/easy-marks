72e85a61ccc338847d584325cd0793db41edf30b
OwenJRJones <owenjrjones@gmail.com>
2024-03-27 20:24:17 -0300

    Basic functionality for getting a text embedding from Azure

---
+++ backend/embedding/embedding.py
@@ -0,0 +1,23 @@
+"""Embedding Module - Azure OpenAI"""
+
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    api_version="2023-05-15",
+    azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT")
+)
+
+
+# Max input tokens = 8192
+# Max array input size = 2048
+def get_embedding(text):
+    response = client.embeddings.create(
+        input=text,
+        model="text-embedding-ada-002"
+    )
+    return response
