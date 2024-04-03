cd7629cf1c1be7b4cd6fd038eee8c42f626c9f87
OwenJRJones <owenjrjones@gmail.com>
2024-04-03 10:38:19 -0300

    Updated Azure env vars to be more specific

--- backend/embedding/embedding.py
+++ backend/embedding/embedding.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = AzureOpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
+    api_key=os.getenv("AZURE_EMBEDDING_API_KEY"),
     api_version="2024-02-01",
-    azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT")
+    azure_endpoint=os.getenv("AZURE_EMBEDDING_ENDPOINT")
 )
 
 
