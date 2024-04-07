d08d0f6d2378ab34d6f050a60f7a992baae8dc39
Laura Abro <l.abrocadabro@gmail.com>
2024-04-07 18:04:45 -0300

    change api version

--- backend/embed/__init__.py
+++ backend/embed/__init__.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 client = AzureOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
-    api_version="2024-04-02",
+    api_version="2024-02-01",
     azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT"),
 )
 
@@ -17,4 +17,4 @@ client = AzureOpenAI(
 # Max array input size = 2048
 def embed(text):
     response = client.embeddings.create(input=text, model="text-embedding-ada-002")
-    return response
+    return response.data[0].embedding
