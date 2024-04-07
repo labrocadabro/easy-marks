9d6297425581498e1301430e18837b1c2e236be2
Laura Abro <l.abrocadabro@gmail.com>
2024-04-07 18:04:35 -0300

    working worker

--- queue_worker.py
+++ queue_worker.py
@@ -5,6 +5,7 @@
 import pika
 import os
 from dotenv import load_dotenv
+
 from backend.scraping import get_website_data
 from backend.summarize import summarize
 from backend.embed import embed
@@ -13,22 +14,33 @@ from backend.database.db_utils import insert
 load_dotenv()
 
 
-url = os.getenv("CLOUDAMQP_URL")
-params = pika.URLParameters(url)
-print(params)
+mq_url = os.getenv("CLOUDAMQP_URL")
+params = pika.URLParameters(mq_url)
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.queue_declare(queue="hello")
 
 
+def trim_left(img_path):
+    index = img_path.find("/frontend")
+    if index != -1:
+        return img_path[index:]
+    else:
+        return img_path
+
+
 def callback(ch, method, properties, body):
-    url = body.decode("utf-8")
-    # title, text, screenshot = get_website_data(url)
-    # summary = summarize(text)
-    # embedding = embed(text)
-    # insert(url, summary, screenshot, embedding, title)
-    print(f" [x] Inserted {url}")
+    try:
+        url = body.decode("utf-8")
+        title, text, screenshot = get_website_data(url)
+        img_path = trim_left(str(screenshot))
+        summary = summarize(text)
+        embedding = embed(text)
+        insert(url, summary, img_path, embedding, title)
+        print(f" [x] Inserted {url}")
+    except Exception as e:
+        print(e)
 
 
 channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=callback)
