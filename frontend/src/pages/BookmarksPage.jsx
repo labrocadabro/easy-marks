a1a8e8f3b9d81b077456c866fb6b63bccaf47b9d
Laura Abro <l.abrocadabro@gmail.com>
2024-04-10 20:53:25 -0300

    fix reversion

--- frontend/src/pages/BookmarksPage.jsx
+++ frontend/src/pages/BookmarksPage.jsx
@@ -11,65 +11,66 @@ function BookmarksPage() {
 	useEffect(() => {
 		if (!searchState) {
 			fetch(`${server}/bookmarks`)
-					.then((res) => res.json())
-					.then((data) => {
-							const bookmarks = JSON.parse(data).map((item) => ({
-									id: item._id._oid,
-									url: item.url,
-									title: item.title,
-									description: item.summary,
-									image: item.screenshot,
-							}));
-							setBookmarksList(bookmarks);
-					})
-					.catch((e) => console.log(e));
-			} else if (sendSearch) {
-				fetch(`${server}/search`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ search: searchState }),
-				})
-					.then((res) => res.json())
-					.then((data) => {
-						const bookmarks = JSON.parse(data).map((item) => ({
-								id: item._id._oid,
-								url: item.url,
-								title: item.title,
-								description: item.summary,
-								image: item.screenshot,
-								score: item.score,
-						}));
-						setBookmarksList(bookmarks);
-						console.log("Booksmarks:", bookmarks)
-						setSendSearch(false);
+				.then((res) => res.json())
+				.then((data) => {
+					const bookmarks = JSON.parse(data).map((item) => ({
+						id: item._id._oid,
+						url: item.url,
+						title: item.title,
+						description: item.summary,
+						image: item.screenshot,
+					}));
+					setBookmarksList(bookmarks);
 				})
 				.catch((e) => console.log(e));
-			}
+		} else if (sendSearch) {
+			fetch(`${server}/search`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ search: searchState }),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					const bookmarks = JSON.parse(data).map((item) => ({
+						id: item._id._oid,
+						url: item.url,
+						title: item.title,
+						description: item.summary,
+						image: item.screenshot,
+						score: item.score,
+					}));
+					setBookmarksList(bookmarks);
+					console.log("Booksmarks:", bookmarks);
+					setSendSearch(false);
+				})
+				.catch((e) => console.log(e));
+		}
 	}, [searchState, sendSearch]);
 
 	const handleSearchStateChange = (value) => {
 		console.log("A search was submitted: " + value);
 		setSearchState(value);
-	}
+	};
 
-	let bookmarkCardsList = []
+	let bookmarkCardsList = [];
 
 	for (const i of bookmarksList) {
-		bookmarkCardsList.push(<BookmarkCard key={i.key} data={i} />)
+		bookmarkCardsList.push(<BookmarkCard key={i.key} data={i} />);
 	}
 
 	return (
 		<>
-
 			<h1 className="text-2xl font-bold mb-4 text-center">View Bookmarks</h1>
 			<SearchBar onSearch={handleSearchStateChange} />
 			<div className="h-56 grid grid-cols-4 gap-5 content-start">
-				{bookmarkCardsList}
+				{bookmarksList
+					? bookmarksList.map((bookmark) => (
+							<BookmarkCard key={bookmark.id} data={bookmark} />
+					  ))
+					: "No bookmarks yet"}
 			</div>
-
-
 		</>
 	);
 }
