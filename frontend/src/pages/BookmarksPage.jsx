f823793e358e12d40e0f024c897e61cadfa41a32
Laura Abro <l.abrocadabro@gmail.com>
2024-04-11 12:38:59 -0300

    fix loading bookmarks and search

--- frontend/src/pages/BookmarksPage.jsx
+++ frontend/src/pages/BookmarksPage.jsx
@@ -4,70 +4,60 @@ import SearchBar from "../components/SearchBar";
 import { server } from "../config/server";
 
 function BookmarksPage() {
-	const [bookmarksList, setBookmarksList] = useState(null);
-	const [searchState, setSearchState] = useState(null);
-	const [sendSearch, setSendSearch] = useState(false);
-	const [updateList, setUpdateList] = useState(false);
+	const [bookmarksList, setBookmarksList] = useState([]);
 
 	useEffect(() => {
-		if (!searchState && !updateList) {
-			fetch(`${server}/api/bookmarks`)
-				.then((res) => res.json())
-				.then((data) => {
-					const bookmarks = JSON.parse(data).map((item) => ({
-						id: item.id,
-						url: item.url,
-						title: item.title,
-						description: item.summary,
-						image: item.screenshot,
-					}));
-					setBookmarksList(bookmarks);
-				})
-				.catch((e) => console.log(e));
-		} else if (sendSearch) {
-			fetch(`${server}/search`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ search: searchState }),
+		fetch(`${server}/api/bookmarks`)
+			.then((res) => res.json())
+			.then((data) => {
+				const bookmarks = JSON.parse(data).map((item) => ({
+					id: item.id,
+					url: item.url,
+					title: item.title,
+					description: item.summary,
+					image: item.screenshot,
+				}));
+				setBookmarksList(bookmarks);
 			})
-				.then((res) => res.json())
-				.then((data) => {
-					const bookmarks = JSON.parse(data).map((item) => ({
-						id: item.id,
-						url: item.url,
-						title: item.title,
-						description: item.summary,
-						image: item.screenshot,
-						score: item.score,
-					}));
-					setBookmarksList(bookmarks);
-					setSendSearch(false);
-				})
-				.catch((e) => console.log(e));
-		}
-	}, [searchState, sendSearch, updateList]);
+			.catch((e) => console.log(e));
+	}, []);
 
-	const handleSearchStateChange = (value) => {
-		setSearchState(value);
-	};
+	function search(value) {
+		fetch(`${server}/search`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ search: value }),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				const bookmarks = JSON.parse(data).map((item) => ({
+					id: item.id,
+					url: item.url,
+					title: item.title,
+					description: item.summary,
+					image: item.screenshot,
+					score: item.score,
+				}));
+				setBookmarksList(bookmarks);
+			})
+			.catch((e) => console.log(e));
+	}
 
 	const updateBookmarks = (deleted_id) => {
 		// Update bookmarks list after delete
 		setBookmarksList(
 			bookmarksList.filter((bookmark) => bookmark.id !== deleted_id)
 		);
-		// Set flag to update bookmarksList
-		setUpdateList(true);
 	};
 
 	return (
 		<>
 			<h1 className="text-2xl font-bold mb-4 text-center">View Bookmarks</h1>
-			<SearchBar onSearch={handleSearchStateChange} />
+			<SearchBar onSearch={search} />
 			<div className="flex flex-wrap gap-5">
-				{bookmarksList?.length
+				{bookmarksList.length
 					? bookmarksList.map((bookmark) => (
 							<BookmarkCard
 								key={bookmark.id}
