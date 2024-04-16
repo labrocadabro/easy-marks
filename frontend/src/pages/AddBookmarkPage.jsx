067fd6b729eb3d69df61fee50fffeca1c0624e8e
Laura Abro <l.abrocadabro@gmail.com>
2024-04-16 17:03:40 -0300

    Remove errant tick mark

--- frontend/src/pages/AddBookmarkPage.jsx
+++ frontend/src/pages/AddBookmarkPage.jsx
@@ -9,7 +9,7 @@ function AddBookmarkPage() {
 
 			<div className="grid">
 				<div style={{marginBottom: "20px", backgroundColor: "#E8D9BF", maxWidth:"100%"}} className="min-w-fit p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">					
-		`			<div className="flex flex-col mx-auto max-w-2xl">
+					<div className="flex flex-col mx-auto max-w-2xl">
 						<h1 className="text-2xl font-bold mb-4 text-center">Add Bookmarks</h1>
 
 						{/* Tabs component to switch between single/bulk add */}
